8dbef8c66e7d35bada6520508f6914845224bbdc
Sara Ginsburg <sara.ginsburg@flatironschool.com>
2022-04-27 16:15:51 -0400

    commit tree 26fd1fd5ad5a92056994b8270718b427af2e383c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee ={
+   
+}
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     return {...employee, [key]:value,}
+
+// }
+const updateEmployeeWithKeyAndValue=(employee, key, value)=>{
+    return {...employee, [key]:value,}
+
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value)=>{
+    employee[key]=value
+    return employee
+}
+
+const deleteFromEmployeeByKey=(employee, key)=>{
+    return {
+        ...employee , [key]:undefined,
+    }  
+}
+
+const destructivelyDeleteFromEmployeeByKey=(employee, key)=>{
+    delete employee[key]
+    return employee
+}
